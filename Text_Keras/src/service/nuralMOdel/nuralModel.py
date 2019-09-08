ee18a969789056fbdbfe753bfd03d4192d52b0af
Swarnendu Chatterjee <swarnendu.chatterjee@traydstream.com>
2019-09-08 08:05:49 +0530

    [LOCAL:created/pushed] Keras Model process build

---
+++ Text_Keras/src/service/nuralMOdel/nuralModel.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+'''
+@Date Of Creation: 
+--------------------------------
+@Author : Swarnendu Chatterjee
+--------------------------------
+@Description About File  :
+
+
+--------------------------------
+
+'''
+
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+
+
+class NuralSMModel:
+
+
+
+    nural_dict=dict()
+
+
+    def __init__(self):
+
+
+
+
+     '''
+     Creating Pickele from Existing Staging Data 
+     
+     '''
+    def _createPickle_(self):
+        df =pd.read_csv('')
+        pass
+
+    '''
+    Process Model building from PICKLE 
+    '''
+    def _readDataSource(self):
+        pass
+
+
+    '''
+    process model for predicting Erros 
+    
+    '''
+    def _processModel(self):
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    np=NuralSMModel()
+
